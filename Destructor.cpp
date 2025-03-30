344dec525268381a001664905cb39605b6284e06
Adityaa-kc <adityakumarchaudhary1623@gmail.com>
2025-03-30 19:32:56 +0530

    Destructor

---
+++ Destructor.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+class Student{
+
+public:
+    string name;
+    double *cgpaPtr;
+
+    Student(string name,double cgpa){
+        this->name = name;
+        cgpaPtr = new double;
+        *cgpaPtr = cgpa;
+    }
+
+    ~Student(){
+        cout<<"Hii I am destructor!!!\n";
+        delete cgpaPtr;
+    }
+};
+
+int main()
+{
+    Student s1("Aditya Chaudhary",10);
+}
