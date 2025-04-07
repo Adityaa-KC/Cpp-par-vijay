4cae6011bf8ef04b0043bf870938dde7f9a3d423
Adityaa-kc <adityakumarchaudhary1623@gmail.com>
2025-04-07 21:14:37 +0530

    New techniques updation

--- ClassesAssignmentquestion.cpp
+++ ClassesAssignmentquestion.cpp
@@ -10,63 +10,72 @@ class Bank{
             cout<<"\nWelcome to AKC bankings ltd. \n"<<endl;
         }
 
-        void set_val()
-        {
-            cout<<"Enter the name of customer :";
-            getline(cin,Name);
-
-            cout<<"Enter the Account no. of customer :";
-            cin>>Accountnum;
-
-            cin.ignore();
-            cout<<"Enter the Account type (Ex : Saving , Current , etc...) of customer :";
-            getline(cin,AccType);
-
-            cout<<"Enter the balance in the account :";
-            cin>>Balance;
-        }
-    
-        void get_info(){
-            cout << "-----------------------------"<<endl;
-            cout<<"Details of the customer is :"<<endl;
-            cout<<"Name of customer :"<<Name<<endl;
-            cout<<"Balance in Account :"<<Balance<<endl;
-        }
-
-        void depo_amt(){
-            cout<<"\nEnter the amount you want to deposit :";
-            cin>>depoAmt;
-            cout<<endl;
-            if (depoAmt < 0)
-            {
-                cout<<"Enter a valid amount to deposit in your bank !!!!";
-            }
-            else
-            {
-                Balance += depoAmt;
-                cout<<"Balance in Account after updating is :"<<Balance<<endl;
-            }
-        }
-
-        void withdraw_amt(){
-            cout<<"\nEnter the amount you want to withdraw :";
-            cin>>withdrawAmt;
-            cout<<endl;
-            if (withdrawAmt>Balance)
-            {
-                cout<<"Not enough amount in your bank !!!!"<<endl;
-            }
-            else 
-            {
-                Balance -= withdrawAmt;
-                cout<<"Balance in Account after withdrawing money is :"<<Balance<<endl;
-            }
-        }
+        void set_val();
+        void get_info();
+        void depo_amt();
+        void withdraw_amt();
 
         ~Bank(){
             cout<<"\nThank you for visiting ";
         }
 };
+
+void Bank :: set_val()
+{
+    cout<<"Enter the name of customer :";
+    getline(cin,Name);
+    cout<<"Enter the Account no. of customer :";
+    cin>>Accountnum;
+    cin.ignore();
+    cout<<"Enter the Account type (Ex : Saving , Current , etc...) of customer :";
+    getline(cin,AccType);
+    cout<<"Enter the balance in the account :";
+    cin>>Balance;
+}
+
+void Bank :: get_info()
+{
+    cout << "-----------------------------"<<endl;
+    cout<<"Details of the customer is :"<<endl;
+    cout<<"Name of customer :"<<Name<<endl;
+    cout<<"Balance in Account :"<<Balance<<endl;
+}
+
+void Bank :: depo_amt()
+{
+    cout<<"\nEnter the amount you want to deposit :";
+    cin>>depoAmt;
+    cout<<endl;
+    if (depoAmt < 0)
+    {
+        cout<<"Enter a valid amount to deposit in your bank !!!!";
+    }
+    else
+    {
+        Balance += depoAmt;
+        cout<<"Balance in Account after updating is :"<<Balance<<endl;
+    }
+}
+
+void Bank :: withdraw_amt()
+{
+    cout<<"\nEnter the amount you want to withdraw :";
+    cin>>withdrawAmt;
+    cout<<endl;
+    if (withdrawAmt < 0)
+    {
+        cout<<"Enter a valid amount !!!!"<<endl;
+    }
+    else if (withdrawAmt > Balance)
+    {
+        cout<<"Not enough amount in your bank !!!!"<<endl;
+    }
+    else 
+    {
+        Balance -= withdrawAmt;
+        cout<<"Balance in Account after withdrawing money is :"<<Balance<<endl;
+    }
+}
 
 int main(){
     Bank c1;
@@ -96,7 +105,6 @@ int main(){
             c1.withdraw_amt();
             break;
         case 4 :
-            cout<<endl;
             cout<<"Quitting ...."<<endl;
             return 0;
         default :
